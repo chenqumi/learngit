f34a2138e385df2a1926b14937cfdbe745e1b03b
chenquan <chenquan@maqgen.com>
2018-06-20 17:12:15 +0800

    row2col.py

---
+++ row2col.py
@@ -0,0 +1,17 @@
+#chenqumi@20180518
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys,os,re
+
+if len(sys.argv) ==1:
+    print("\nUsage: {} <>".format(sys.argv[0]))
+    sys.exit()
+
+file = sys.argv[1]
+
+sam = []
+with open(file) as fd:
+    for line in fd:
+                sam = line.strip().split("\t")
+for i in sam:
+        print("{0}".format(i))
